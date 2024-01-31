0545605e9994159c75bfb5766df39c286a1be3f8
a-martinb <a.martinb@edu.uah.es>
2024-01-31 20:08:13 +0100

    añadir estadisticas al parar ejecucuion del cliente  noc_client.py

--- noc_client.py
+++ noc_client.py
@@ -67,8 +67,10 @@ def main():
 
         # Esperar un tiempo antes de enviar el próximo paquete (opcional)
         time.sleep(1)
+    try:
+    # Código que puede generar excepciones
     except KeyboardInterrupt:
-        signal_handler(signal.SIGINT, None)
-
+    # Manejo de la interrupción del teclado
+    signal_handler(signal.SIGINT, None)
 if __name__ == "__main__":
     main()
